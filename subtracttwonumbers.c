ad57de3fae89a9de4d38d711b405d9b023bf363f
vanshikavithlani857-cloud <vanshikavithlani857@gmail.com>
2025-08-08 10:56:06 +0530

    Add files via upload

---
+++ subtracttwonumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,subtract;
+    printf("First Number :");
+    scanf("%d",&num1);
+
+    printf("Second Number :");
+    scanf("%d",&num2);
+
+    subtract=num1-num2;
+
+    printf("The subtraction of two values is : %d", subtract);
+    return 0;
+}
