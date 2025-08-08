107c6778e14510fb378722012a73bdc6ee4e1c16
vanshikavithlani857-cloud <vanshikavithlani857@gmail.com>
2025-08-08 10:51:48 +0530

    Add files via upload

---
+++ addtwonumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("First Number :");
+    scanf("%d",&num1);
+
+    printf("Second Number :");
+    scanf("%d",&num2);
+
+    sum=num1+num2;
+
+    printf("The sum of two values : %d", sum);
+    return 0;
+}
