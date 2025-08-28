e7990dc804f5eb97a6228cbb1bfd368e34374c12
Vanshika Vithlani <vanshikavithlani857@gmail.com>
2025-08-28 19:26:22 +0530

    Add files via upload

--- ifelse6.c
+++ ifelse6.c
@@ -8,30 +8,26 @@ int main()
     total=s1+s2+s3;
     avg=total/3;
     printf("The total and average marks of three subjects is %d %d \n",total,avg);
-    if(avg>=70)
+    if(avg>=70 && s1>35 && s2>35 && s3>35)
     {
         printf("Student got destinction \n");
     }
-    else if(avg>=60)
+    else if(avg>=60 && s1>35 && s2>35 && s3>35)
     {
         printf("Student got first class \n");
     }
-    else if(avg>=50)
+    else if(avg>=50 && s1>35 && s2>35 && s3>35)
     {
         printf("Student got second class\n");
 
     }
-    else if(avg>=35)
+    else if(avg>=35 && s1>35 && s2>35 && s3>35)
     {
         printf("Student got third class\n");
     }
     else 
     {
         printf("Student got failed \n");
-    }
-    if(s1<35 || s2<35 || s3<35)
-    {
-        printf("Student failed the exam");
     }
     return 0;
 }
